6d56d1bbddb726df889250849f539bfdf62dba67
nyrkovaana <nyrkovaana@gmail.com>
2024-11-25 23:28:00 +0400

    Add main function Update .gitignore

---
+++ main.py
@@ -0,0 +1,15 @@
+def check_category(line):
+    parts = line.strip().split(',')
+    return bool(parts[2]) if len(parts) > 2 else False
+
+
+def process_line(line, result_file_handler):
+    if check_category(line):
+        result_file_handler.write(line)
+
+
+if __name__ == '__main__':
+    with open('visits_log.csv', 'r') as log_file:
+        with open('funnel.csv', 'w', encoding='utf-8') as results_file:
+            for line in log_file:
+                process_line(line, results_file)
